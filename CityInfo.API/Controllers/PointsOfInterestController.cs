83fe07b01db923fa03d8d5231ffe1be902690b1a
Katherine Picazo <kpicazo@kruxanalytics.com>
2023-03-14 23:05:18 -0600

    Add POST endpoint to add a point of interest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -21,7 +21,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -39,6 +39,52 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId,  
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+            // First, check to see if user is trying to add a point of interest to a city that does not exist.
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null )
+            {
+                return NotFound();
+            }
+
+            // Calculate the ID of the new point of interest.
+            // For now, just get the highest ID of all existing points of interest, and add 1 to it.
+            // Note: iterating through all the cities and their points just to get an ID is not ideal for performance reasons,
+            // and also this code does not take possible errors into account when multiple consumers try to get an ID at the same time.
+            // We'll improve this later.
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            // Map PointOfInterestForCreationDto to PointOfInterestDto since that's the object type in the data store.
+            // Note: this is not ideal as mapping can lead to errors.
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+
+            // Add point of interest to city.
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            // Finally, return something.
+            // The advised response for a POST is "201 Created". Can do this with helper method CreatedAtRoute.
+            // This allows us to return a response with the location header which contains the URI where the 
+            // newly created point of interest can be found.
+            return CreatedAtRoute("GetPointOfInterest", 
+                new
+                {
+                    // these are the values that the specified route template needs
+                    cityId = cityId,
+                    pointOfInterestId = finalPointOfInterest.Id
+                },
+                finalPointOfInterest
+            );
         }
     }
 }
---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CityInfo.API.Models
+{
+    // This is the DTO used to create a new point of interest.
+    // We want to keep this separate from the DTO used to retrieve a point of interest from the database.
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
