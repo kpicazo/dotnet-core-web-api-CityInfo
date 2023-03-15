bdaf3dee70cc2e37c3750c0e90597202d6418b43
Katherine Picazo <kpicazo@kruxanalytics.com>
2023-03-14 23:15:02 -0600

    Add annotations for point of interest model validation

--- CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,10 +1,16 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     // This is the DTO used to create a new point of interest.
     // We want to keep this separate from the DTO used to retrieve a point of interest from the database.
     public class PointOfInterestForCreationDto
     {
-        public string Name { get; set; }
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
